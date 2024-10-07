6342ac97b99359c3c65b744c77dc831d1b65ee1e
Ghost-W0LF <Abhinab1221@gmail.com>
2024-10-07 11:02:00 +0545

    fix: getter function for TextTheme

--- lib/base/theme/custome_theme/app_text_theme.dart
+++ lib/base/theme/custome_theme/app_text_theme.dart
@@ -27,28 +27,34 @@ class AppTextTheme {
         fontSize: 14.0.sp,
         fontWeight: FontWeight.w600,
         color: kcPrimaryTextColor),
-    //button
+    titleSmall: const TextStyle().copyWith(
+        fontSize: 16.0.sp,
+        fontWeight: FontWeight.w400,
+        color: kcPrimaryTextColor),
+    // body
 
+    bodyLarge: const TextStyle().copyWith(
+        fontSize: 14.0.sp,
+        fontWeight: FontWeight.w500,
+        color: kcPrimaryTextColor),
+    bodyMedium: const TextStyle().copyWith(
+        fontSize: 14.0.sp,
+        fontWeight: FontWeight.normal,
+        color: kcPrimaryTextColor),
+    bodySmall: const TextStyle().copyWith(
+        fontSize: 14.0.sp,
+        fontWeight: FontWeight.w500,
+        color: kcPrimaryTextColor.withOpacity(0.5)),
+
+    //button
     labelLarge: const TextStyle().copyWith(
         fontSize: 12.0.sp,
         fontWeight: FontWeight.normal,
         color: kcPrimaryTextColor),
-    // titleSmall: const TextStyle().copyWith(
-    //     fontSize: 16.0, fontWeight: FontWeight.w400, color: Colors.black),
-    // bodyLarge: const TextStyle().copyWith(
-    //     fontSize: 14.0, fontWeight: FontWeight.w500, color: Colors.black),
-    // bodyMedium: const TextStyle().copyWith(
-    //     fontSize: 14.0, fontWeight: FontWeight.normal, color: Colors.black),
-    // bodySmall: const TextStyle().copyWith(
-    //     fontSize: 14.0,
-    //     fontWeight: FontWeight.w500,
-    //     color: Colors.black.withOpacity(0.5)),
-    // labelLarge: const TextStyle().copyWith(
-    //     fontSize: 12.0, fontWeight: FontWeight.normal, color: Colors.black),
-    // labelMedium: const TextStyle().copyWith(
-    //     fontSize: 12.0,
-    //     fontWeight: FontWeight.normal,
-    //     color: Colors.black.withOpacity(0.5)),
+    labelMedium: const TextStyle().copyWith(
+        fontSize: 12.0.sp,
+        fontWeight: FontWeight.normal,
+        color: kcPrimaryTextColor.withOpacity(0.5)),
   );
 
   static TextTheme darkTextTheme = TextTheme(
@@ -65,30 +71,54 @@ class AppTextTheme {
 
     labelLarge: const TextStyle().copyWith(
         fontSize: 12.0.sp, fontWeight: FontWeight.normal, color: Colors.white),
-
-    // headlineMedium: const TextStyle().copyWith(
-    //     fontSize: 24.0, fontWeight: FontWeight.w600, color: Colors.white),
-    // headlineSmall: const TextStyle().copyWith(
-    //     fontSize: 18.0, fontWeight: FontWeight.w600, color: Colors.white),
-    // titleLarge: const TextStyle().copyWith(
-    //     fontSize: 16.0, fontWeight: FontWeight.w600, color: Colors.white),
-    // titleMedium: const TextStyle().copyWith(
-    //     fontSize: 16.0, fontWeight: FontWeight.w500, color: Colors.white),
-    // titleSmall: const TextStyle().copyWith(
-    //     fontSize: 16.0, fontWeight: FontWeight.w400, color: Colors.white),
-    // bodyLarge: const TextStyle().copyWith(
-    //     fontSize: 14.0, fontWeight: FontWeight.w500, color: Colors.white),
-    // bodyMedium: const TextStyle().copyWith(
-    //     fontSize: 14.0, fontWeight: FontWeight.normal, color: Colors.white),
-    // bodySmall: const TextStyle().copyWith(
-    //     fontSize: 14.0,
-    //     fontWeight: FontWeight.w500,
-    //     color: Colors.white.withOpacity(0.5)),
-    // labelLarge: const TextStyle().copyWith(
-    //     fontSize: 12.0, fontWeight: FontWeight.normal, color: Colors.white),
-    // labelMedium: const TextStyle().copyWith(
-    //     fontSize: 12.0,
-    //     fontWeight: FontWeight.normal,
-    //     color: Colors.white.withOpacity(0.5)),
   );
+
+  /// Getter function for text theme
+  ///     //headline
+  static headlineLarge(BuildContext context) {
+    return Theme.of(context).textTheme.headlineLarge;
+  }
+
+  static headlineMedium(BuildContext context) {
+    return Theme.of(context).textTheme.headlineMedium;
+  }
+
+  static headlineSmall(BuildContext context) {
+    return Theme.of(context).textTheme.headlineSmall;
+  }
+
+  //Title
+  static titleLarge(BuildContext context) {
+    return Theme.of(context).textTheme.titleLarge;
+  }
+
+  static titleMedium(BuildContext context) {
+    return Theme.of(context).textTheme.titleMedium;
+  }
+
+  static titleSmall(BuildContext context) {
+    return Theme.of(context).textTheme.titleSmall;
+  }
+
+  // Body
+  static bodyLarge(BuildContext context) {
+    return Theme.of(context).textTheme.titleLarge;
+  }
+
+  static bodyMedium(BuildContext context) {
+    return Theme.of(context).textTheme.bodyMedium;
+  }
+
+  static bodySmall(BuildContext context) {
+    return Theme.of(context).textTheme.bodySmall;
+  }
+
+  // lable
+  static labelLarge(BuildContext context) {
+    return Theme.of(context).textTheme.labelLarge;
+  }
+
+  static labelMedium(BuildContext context) {
+    return Theme.of(context).textTheme.labelMedium;
+  }
 }
